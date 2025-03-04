eaeec1912239d1bb7fb7fc212aaf4f946fc0a8f8
jgarct04 <91263644+jgarct04@users.noreply.github.com>
2025-03-04 19:45:24 +0100

    Update TradewingApplication.java

--- src/main/java/com/tradewing/TradewingApplication.java
+++ src/main/java/com/tradewing/TradewingApplication.java
@@ -2,12 +2,20 @@ package com.tradewing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TradewingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TradewingApplication.class, args);
-	}
+  @RequestMapping("/")
+  public String home() {
+    return "Hello Docker World";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
