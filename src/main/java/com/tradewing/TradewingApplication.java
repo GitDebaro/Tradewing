adac541aff3ebc87ec0e4f4bde78b2a23f83db79
jgarct04 <91263644+jgarct04@users.noreply.github.com>
2025-03-04 19:44:15 +0100

    Init com

---
+++ src/main/java/com/tradewing/TradewingApplication.java
@@ -0,0 +1,13 @@
+package com.tradewing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradewingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradewingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradewing/TradewingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradewing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradewingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
