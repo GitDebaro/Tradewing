b80d7cc9c89784259deed15cdf64158bf34d7ae1
GitDebaro <pbaroe00@estudiantes.unileon.es>
2025-05-31 11:55:37 +0200

    Register functionality tests

--- src/test/java/com/tradewing/servicetests/UserServiceImplTest.java
+++ src/test/java/com/tradewing/servicetests/UserServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.tradewing.servicetests;
 
+import org.springframework.http.ResponseEntity;
+
 import com.tradewing.models.UserEntity;
 import com.tradewing.services.impl.UserServiceImpl;
 import com.tradewing.repos.UserRepo;
@@ -11,6 +13,7 @@ import org.mockito.InjectMocks;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
@@ -36,7 +39,6 @@ class UserServiceImplTest {
         testUser.setName("Test User");
         testUser.setEmail("test@user.com");
         testUser.setPassword("sha256");
-
     }
 
     @Test
@@ -61,5 +63,68 @@ class UserServiceImplTest {
         assertTrue(result.isEmpty());
         verify(usrRepo).findAll();
     }
+
+    @Test   
+    void addUserSuccess(){
+        UserEntity newUser = new UserEntity();        
+        newUser.setId(2L);
+        newUser.setName("Test User 2");
+        newUser.setEmail("test2@user.com");
+        newUser.setPassword("sha256");
+
+        when(usrRepo.save(any(UserEntity.class))).thenReturn(newUser);
+
+        ResponseEntity<?> response = userSC.addUser(newUser);
+        assertEquals(201, response.getStatusCodeValue());
+        assertEquals("User created successfully", response.getBody());
+        verify(usrRepo).save(any(UserEntity.class));
+    }
+
+    @Test
+    void addUserConflict(){
+        //Same user that was defined in the mock
+        UserEntity newUser = new UserEntity();        
+        newUser.setId(2L);
+        newUser.setName("Test User");
+        newUser.setEmail("test@user.com");
+        newUser.setPassword("sha256");
+
+        when(usrRepo.save(any(UserEntity.class))).thenThrow(new RuntimeException("duplicate key value violates unique constraint"));
+
+        ResponseEntity<?> response = userSC.addUser(newUser);
+        assertEquals(409, response.getStatusCodeValue());
+        assertEquals("There is already an account linked to this email", response.getBody());
+        verify(usrRepo).save(any(UserEntity.class));
+    }
+
+    @Test 
+    void addUserDDBBNotAccesible(){        
+        UserEntity newUser = new UserEntity();        
+        newUser.setId(2L);
+        newUser.setName("Test User 2");
+        newUser.setEmail("test2@user.com");
+        newUser.setPassword("sha256");
+
+        when(usrRepo.save(any(UserEntity.class))).thenThrow(new RuntimeException("Database maximum client number reached"));
+
+        ResponseEntity<?> response = userSC.addUser(newUser);
+        assertEquals(500, response.getStatusCodeValue());
+        assertEquals("An error ocurred while creating a new User. Try again later", response.getBody());
+        verify(usrRepo).save(any(UserEntity.class));
+    }
+
+    /*
+    EXPECTED REFACTOR of Controller and Service
+    TO DO: loginUserTests
+
+    TO DO: userData tests
+
+    EXPECTED REFACTOR: JWT Utils class 
+
+    TO DO: myInventory
+
+    TO DO: updateUser
+    */
+    
 
 }
